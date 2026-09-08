9147f902a4ea283cdf8afeae8f9bd88b0ba65a82
devsharma984 <dmexpert003@gmail.com>
2026-09-08 14:45:05 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,13 @@
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
   <link rel="manifest" href="/site.webmanifest">
   <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KMGZ7PN6');</script>
+<!-- End Google Tag Manager -->
 
   @php
     $currentPath = request()->path() === '/' ? '/' : '/' . request()->path();
@@ -65,6 +72,10 @@
 </head>
 
 <body class="tt-smooth-scroll">
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMGZ7PN6"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
   <!-- Preloader -->
   <div id="preloader">
